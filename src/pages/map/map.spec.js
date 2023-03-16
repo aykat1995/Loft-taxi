5c39eb27041241b6bfc9c43d378efe426427fcf5
aykat1995 <54204536+aykat1995@users.noreply.github.com>
2023-03-17 00:14:05 +1100

    build

--- src/pages/map/map.spec.js
+++ src/pages/map/map.spec.js
@@ -3,20 +3,23 @@ import Map from './mapPage.jsx'
 import mapboxgl from "mapbox-gl";
 import '@testing-library/jest-dom';
 
-jest.mock("mapbox-gl");
-
 const removeHandle = jest.fn();
 const mapImplement = jest.fn(() => ({
   on: jest.fn(),
   remove: removeHandle
 }))
 mapboxgl.Map.mockImplementation(mapImplement);
+const store = createStore();
 
 describe("Map", () => {
   describe("#MapRender", () => {
-    it("Map renders correctly", () => {
-      const { container } = render(<Map />);
-      expect(container.getElementsByClassName("map__wrapper").length).toBeTruthy();
+    it("is Map renders correctly", () => {
+      const { container } = render(
+        <Provider store={store}>
+          <Map />
+        </Provider>
+      );
+      expect(container.getElementsByClassName("map-container").length).toBeTruthy();
     })
   });
 });
